cb091f813e0e9eadd31290c6c806fd2ea1abff04
Angel Neyra <201721559@tese.edu.mx>
2022-09-02 00:44:33 -0500

    Modificacion final conexión

--- src/main/java/MySQL/Java.java
+++ src/main/java/MySQL/Java.java
@@ -5,15 +5,29 @@
  */
 package MySQL;
 
-/**
- *
- * @author M0KUM4
- */
-public class Java extends javax.swing.JFrame {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    /**
-     * Creates new form Java
-     */
+public class Java extends javax.swing.JFrame {
+    Connection con;
+    String driver="com.mysql.cj.jdbc.Driver";
+    String user="root";
+    String pass="201721559";
+    String url="jdbc:mysql://localhost:3306/sistemacursos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public void conectar(){
+        con=null;
+        try {
+            Class.forName(driver);
+            con=(Connection) DriverManager.getConnection(url,user,pass);
+            if(con!=null){
+                estadoCon.setText("Conexion exitosa");
+            }
+        } catch (Exception e) {
+            estadoCon.setText("Conexion no exitosa "+e);
+        }
+    }
+
     public Java() {
         initComponents();
     }
@@ -27,21 +41,47 @@ public class Java extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        estadoCon = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        estadoCon.setText("jLabel1");
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(estadoCon))
+                .addContainerGap(304, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(estadoCon)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(222, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+     conectar();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +119,7 @@ public class Java extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel estadoCon;
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
